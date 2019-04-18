95556b87a2ff9b49acba4d0ceaa0a196062ffa9a
J <j@jake.local>
2019-04-17 21:17:35 -0700

    Initial summit from Jake

---
+++ src/LeaderBoardScreen.java
@@ -0,0 +1,39 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class BackGround here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class LeaderBoardScreen extends World
+{
+
+    /**
+     * Constructor for objects of class BackGround.
+     * 
+     */
+    public LeaderBoardScreen()
+    {    
+        // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
+        super(600, 400, 1);
+        prepare();
+    }
+    
+        private void prepare()
+    {
+        
+        Label label = new Label("LEADER BOARD");
+        addObject(label, 280, 75);
+        
+        UserInfo userinfo = new UserInfo();
+        addObject(userinfo,180,125);  
+        
+        UserInfo userinfo2 = new UserInfo();
+        addObject(userinfo2,180,225);
+        
+        UserInfo userinfo3 = new UserInfo();
+        addObject(userinfo3,180,325);
+    }
+    
+}
