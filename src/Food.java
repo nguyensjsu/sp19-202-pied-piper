56563428946aa9afb43da5558706e3abd6358742
HaoRanChen123 <47268227+HaoRanChen123@users.noreply.github.com>
2019-04-26 00:46:26 -0700

    1

---
+++ src/Food.java
@@ -0,0 +1,46 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+import java.util.ArrayList;
+import java.util.List;
+/**
+ * Write a description of class Food here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Food extends Item implements Subject
+{
+  
+    List<Board> list= new ArrayList <Board>();
+    /**
+     * Act - do whatever the Food wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act() 
+    {
+        if(isTouching(Snake.class)){
+            Greenfoot.playSound("Eat.mp3");
+            
+            mynotify();
+            
+        }  
+        super.act();
+    }  
+    public void attach(Board board){
+        list.add(board);
+    };
+    public void remove(){};
+    public void mynotify(){
+          for (int i=0;i<list.size();i++){
+              if (list.get(i) instanceof FoodBoard){
+                  FoodBoard foodBoard=(FoodBoard) list.get(i);
+                  foodBoard.update(this);
+              }
+              else if (list.get(i) instanceof ScoreBoard){
+                  ScoreBoard scoreBoard=(ScoreBoard) list.get(i);
+                  scoreBoard.update(this);
+              }
+          }
+
+     
+    };
+}
