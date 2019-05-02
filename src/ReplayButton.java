854654ad46a291d0a38eb43c0ed2138e1c3a176a
J <j@jake.local>
2019-05-02 12:07:28 -0700

    Changing score as a singleton pattern.
    1. Change score as a singleton pattern.
    2. Leaderboard get score from ScoreBoard.java
    3. Change Icons of leaderboard.

---
+++ src/ReplayButton.java
@@ -0,0 +1,22 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class ReplayButton here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class ReplayButton extends Actor
+{
+    /**
+     * Act - do whatever the ReplayButton wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+      private GreenfootImage buttonImage;
+
+    public ReplayButton(){
+        buttonImage = new GreenfootImage("replay.png");
+        buttonImage.scale(80,50);
+        setImage(buttonImage);
+    } 
+}
