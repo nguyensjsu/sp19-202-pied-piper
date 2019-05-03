c2c1e38ee26fa3fe84f530b6d8624048f3fe5847
Yueqiao Zhang <yueqiaozhang79@gmail.com>
2019-05-03 00:58:42 -0700

    added ISnakeComponent for snake decorator design pattern

---
+++ src/ISnakeComponent.java
@@ -0,0 +1,9 @@
+import greenfoot.*; 
+
+public interface ISnakeComponent
+{
+
+    Color snakeDecoratorColor(); // return snake color						
+    int snakeDecoratorSpeed();	// return snake speed
+    int snakeDecoratorLife();  // return snake lifespan
+}
