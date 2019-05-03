34312b47dbf2cc4fe0ed97aa8df550d240e5d6f0
Prachi Jani <47283283+PrachiJani13@users.noreply.github.com>
2019-05-03 07:21:21 -0700

    Add files via upload

---
+++ src/MyWorld.java
@@ -0,0 +1,59 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class MyWorld here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class MyWorld extends World
+{
+    /**
+     * Constructor for objects of class MyWorld.
+     * 
+     */
+    public MyWorld()
+    {    
+        // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
+
+       
+        super(1200, 700, 1); 
+        GreenfootImage tile = new GreenfootImage(50,50);
+        tile.drawRect(0,0,50,50);
+        setBackground(tile);
+        prepare();
+    }
+
+    /**
+     * Prepare the world for the start of the program.
+     * That is: create the initial objects and add them to the world.
+     */
+    private void prepare()
+    {
+        //addObject(new Snake(), 125, 125);
+        this.setPaintOrder(AchievementBoard.class,NumberSpace.class,FoodBoard.class,PowerUpBoard.class,ScoreBoard.class);
+        
+        //Snake snake=new Snake();
+        Board board=new Board();
+        PowerUpBoard powerupboard=new  PowerUpBoard();
+        FoodBoard foodboard=new FoodBoard();
+        ScoreBoard scoreboard=new ScoreBoard();
+        AchievementBoard achievementboard = new AchievementBoard();
+        Food food = new Food();
+        food.attach(foodboard);
+        foodboard.register(achievementboard);
+        food.attach(scoreboard);
+        addObject(food,525,125);
+        PowerUp powerUp = new PowerUp();
+        powerUp.attach(powerupboard);
+        powerUp.attach(scoreboard);
+        powerupboard.register(achievementboard);
+        addObject(board,600,0);
+        addObject(powerUp,375,175);
+        addObject(foodboard,50,10);
+        addObject(powerupboard,1150,10);
+        addObject(scoreboard,600,10);
+        addObject(achievementboard, 300 , 350);
+        achievementboard.setLocation(600, 600);
+    }
+}
---
+++ src/PowerUpBoard.java
@@ -0,0 +1,83 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+
+import java.util.ArrayList;
+import java.util.List;
+/**
+     * Write a description of class FoodBoard here.
+     * 
+     * @author (HaoRan Chen) 
+     * @version (4/19)
+     */
+public class PowerUpBoard extends Board implements ISubject
+{
+    /**
+     * Act - do whatever the PowerUpBoard wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    private Number_Image number_image;
+    
+    List<Board> list= new ArrayList <Board>();
+    public PowerUpBoard(){
+     Color bgColor = new Color(0, 0, 0, 0);
+     GreenfootImage bg=new GreenfootImage("speed:",40,Color.RED,bgColor);
+     bg.scale(100,50);
+     setImage(bg);
+      
+    }
+    
+    public void act() 
+    {
+      
+    }
+    
+    
+    public void update(Item item){
+     PowerUp powerup=(PowerUp)item;
+        
+     powerup.setCount(powerup.getCount()+1);
+     
+     if(powerup.getCount() == 5)
+     {
+        notifyObservers(item) ;
+     }
+    
+     show(powerup.getCount());
+    
+    } public void show(int count){
+       
+     Count_Number_Factory number_factory=new Count_Number_Factory();
+     number_image=number_factory.makeImage(count);
+     ArrayList list=number_image.getmyImage();
+     GreenfootImage units=(GreenfootImage) list.get(0);
+     GreenfootImage digits=(GreenfootImage)list.get(1);
+     world=this.getWorld();
+     
+     unitsspace=new NumberSpace(units);
+     unitsspace.set_the_Image();
+     digitsspace=new NumberSpace(digits);
+     digitsspace.set_the_Image();
+     
+     world.addObject(unitsspace,1175,70);
+     world.addObject(digitsspace,1125,70);
+    }
+    
+    public void register(Board board)
+    {
+        list.add(board);
+    }
+    
+    public void unregister(IObserver obj)
+    {
+    }
+    
+    public void notifyObservers(Item item)
+    {
+        AchievementBoard achievementBoard=(AchievementBoard)list.get(0);
+        
+        achievementBoard.update(item);
+    }
+    
+    
+    
+}
