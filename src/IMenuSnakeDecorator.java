047838c64068b91f5a75bb747f9b1d9847ce95c9
athsueh <athsueh@ucsc.edu>
2019-05-03 18:18:22 -0700

    Create IMenuSnakeDecorator.java

---
+++ src/IMenuSnakeDecorator.java
@@ -0,0 +1,19 @@
+import greenfoot.*;
+
+/**
+ * Write a description of class menuSnakeDecorator here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public interface IMenuSnakeDecorator 
+{
+    /**
+     * An example of a method - replace this comment with your own
+     * 
+     * @param  y   a sample parameter for a method
+     * @return     the sum of x and y 
+     */
+    public GreenfootImage getImg();
+
+}
